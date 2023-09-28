10e51531ac2c2855ccd89150046daea42e909836
Madalina <madalinacocian@yahoo.com>
2023-09-28 14:55:34 +0300

    Update UFTExecutor.ps1

--- test-execution/UFTExecutor.ps1
+++ test-execution/UFTExecutor.ps1
@@ -5,7 +5,7 @@ Set-Content -Path ./build/testsToRun.mtbx -Value $TESTS_TO_RUN_XML
 Set-Content -Path ./build/Props.txt -Value "runType=FileSystem"
 Add-Content -Path ./build/Props.txt -Value "resultUnifiedTestClassname=true"
 Add-Content -Path ./build/Props.txt -Value "resultsFilename=build\\Results.xml"
-Add-Content -Path ./build/Props.txt -Value "Test1=$WORKSPACE_ESCAPED\\test-execution\\build\\testsToRun.mtbx"
+Add-Content -Path ./build/Props.txt -Value "Test1=\"$WORKSPACE_ESCAPED\\test-execution\\build\\testsToRun.mtbx\""
 Get-Content -Path ./build/Props.txt
 Start-Process -FilePath "FTToolsLauncher.exe" -ArgumentList "-paramfile $PWD/build/Props.txt" -Wait *>&1
 $resultsContent = Get-Content -Path ./build/Results.xml
