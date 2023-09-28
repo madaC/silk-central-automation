006d86ae8addde946a57c66ecceac27517e9495a
Madalina <madalinacocian@yahoo.com>
2023-09-28 14:41:20 +0300

    Update UFTExecutor.ps1

--- test-execution/UFTExecutor.ps1
+++ test-execution/UFTExecutor.ps1
@@ -7,7 +7,7 @@ Add-Content -Path ./build/Props.txt -Value "resultUnifiedTestClassname=true"
 Add-Content -Path ./build/Props.txt -Value "resultsFilename=build\\Results.xml"
 Add-Content -Path ./build/Props.txt -Value "Test1=$WORKSPACE_ESCAPED\\test-execution\\build\\testsToRun.mtbx"
 Get-Content -Path ./build/Props.txt
-Start-Process -FilePath "FTToolsLauncher.exe" -ArgumentList "-paramfile $PWD/build/Props.txt" -Wait *>&1
+Start-Process -FilePath "FTToolsLauncher.exe" -ArgumentList "-paramfile $PWD/build/Props.txt" -Wait *>&5
 $resultsContent = Get-Content -Path ./build/Results.xml
 $WORKSPACE_PATH = -join($WORKSPACE_ESCAPED, "\\test-execution\\");
 $newResultContent = $resultsContent -replace('/','\') -replace ($WORKSPACE_PATH, '') -replace ('file:\\\\\\', '') -replace ('<\\','</') -replace ('\\>','/>') -replace('xmlns:xsi="[^"]*"','') -replace('xmlns:xsd="[^"]*"','') -replace ('package=\"FTToolsLauncher\"','')
