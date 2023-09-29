ed7326b712bcde3e358e03d4d8b8feb926994e9d
Madalina <madalinacocian@yahoo.com>
2023-09-29 16:07:46 +0300

    Update UFTExecutor.ps1

--- test-execution/UFTExecutor.ps1
+++ test-execution/UFTExecutor.ps1
@@ -10,6 +10,8 @@ Add-Content -Path ./build/Props.txt -Value "Test1=$WORKSPACE_ESCAPED2\\test-exec
 Get-Content -Path ./build/Props.txt
 Start-Process -FilePath "FTToolsLauncher.exe" -ArgumentList "-paramfile `"$PWD/build/Props.txt`"" -Wait *>&1
 $resultsContent = Get-Content -Path ./build/Results.xml
-$WORKSPACE_PATH = -join($WORKSPACE_ESCAPED2, "\\test-execution\\");
-$newResultContent = $resultsContent -replace('/','\') -replace ($WORKSPACE_PATH, '') -replace ('file:\\\\\\', '') -replace ('<\\','</') -replace ('\\>','/>') -replace('xmlns:xsi="[^"]*"','') -replace('xmlns:xsd="[^"]*"','') -replace ('package=\"FTToolsLauncher\"','')
+$WORKSPACE_PATH = -join($WORKSPACE_ESCAPED, "\\test-execution\\");
+$WORKSPACE_PATH2 = -join($WORKSPACE_ESCAPED2, "\\test-execution\\");
+$newResultContent = $resultsContent -replace('/','\') -replace ($WORKSPACE_PATH, '') -replace ($WORKSPACE_PATH2, '') -replace ('file:\\\\\\', '') -replace ('<\\','</') -replace ('\\>','/>') -replace('xmlns:xsi="[^"]*"','') -replace('xmlns:xsd="[^"]*"','') -replace ('package=\"FTToolsLauncher\"','') 
+
 $newResultContent | Set-Content -Path ./build/Results.xml
